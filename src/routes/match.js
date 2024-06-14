7e0bc96bd6a817400dabf6cfc6267c50f14e169f
Sina Sadigh <42268464+sinasadigh@users.noreply.github.com>
2024-06-14 21:24:43 +0300

    Update match.js

--- src/routes/match.js
+++ src/routes/match.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const Matches = require("../models/match");
-
+const Application = require("../models/application");
 const { authenticateUser } = require("../../middlewares/autenticateUser");
 
 router.get("/", authenticateUser, async (req, res) => {
   try {
     const { user } = req;
 
-    const matches = await Matches.find({
-      intern_id: user.id, // Assuming user._id matches intern_id
+    const matches = await Matches.findOne({
+      $or: [{ intern_id: user.intern }, { intern_id: user.id }],
     });
-
-    res.status(200).json({ matches });
+    matchArray = [];
+    for (let m of matches.matches) {
+      let isApplied = false;
+      const application = await Application.exists({
+        advert: m.advert_id._id,
+        intern_id: user.intern._id,
+      });
+      if (application) {
+        isApplied = true;
+      }
+      let obj = { ...m.toObject(), isApplied };
+      matchArray.push(obj);
+    }
+    res.status(200).json({ matches: matchArray });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
